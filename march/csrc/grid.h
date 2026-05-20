3f0b539eb3ee93aa0e551e966398e3c51ffc483c
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-20 13:34:24 +0100

    feat: add radius parameter to pc_to_voxel_grid function signature

--- march/csrc/grid.h
+++ march/csrc/grid.h
@@ -15,6 +15,7 @@ namespace grid {
         IndexType res_z, 
         int k_threshold,
         int num_keep,
+        float r,
         Vertex<Scalar>** out_vertices,
         IndexType* out_num_vertices,
         IndexType** out_cubes,
