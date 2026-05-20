076babb5360208dfc97341270d5c591a93088d8d
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-20 18:34:46 +0100

    chore: minor Python wrapper adjustments in module/mc.py

--- march/module/mc.py
+++ march/module/mc.py
@@ -1,19 +1,22 @@
 import torch
-from torch import nn
+from torch import dtype, nn
 import torch.nn.functional as F
 from torch.autograd import Function
 
-from .._C import MCF
+from .._C import MCFI, MCFL
 
 
 class DMC(nn.Module):
-    def __init__(self, dtype=torch.float32):
+    def __init__(self, vdtype=torch.float32, cdtype=torch.int32):
         super().__init__()
-        self.dtype = dtype
-        if dtype == torch.float32:
-            mc = MCF()
+        self.vdtype = vdtype
+        self.cdtype = cdtype
+        if cdtype == torch.int32:
+            mc = MCFI()
+        elif cdtype == torch.int64:
+            mc = MCFL()
         else:
-            raise NotImplementedError(f"Unsupported dtype: {dtype}")
+            raise NotImplementedError(f"Unsupported dtype: {cdtype}")
             
         class DMCFunction(Function):
             @staticmethod
@@ -37,8 +40,8 @@ class DMC(nn.Module):
     
     def forward(self, grid_vertices, cubes, values, iso):
         if values.min() >= iso or values.max() <= iso:
-            return torch.zeros((0, 3), dtype=self.dtype, device=grid_vertices.device), \
-                torch.zeros((0, 3), dtype=torch.int32, device=grid_vertices.device)
+            return torch.zeros((0, 3), dtype=self.vdtype, device=grid_vertices.device), \
+                torch.zeros((0, 3), dtype=self.cdtype, device=grid_vertices.device)
         
         verts, tris = self.func.apply(grid_vertices, cubes, values, iso)
 
