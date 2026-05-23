42765168aa54ff0506cefe19c5c1552e5f1ae376
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-23 21:56:23 +0100

    feat(cuda): add header declaration for pc_to_voxel_grid_chunk

--- march/csrc/grid.h
+++ march/csrc/grid.h
@@ -22,4 +22,22 @@ namespace grid {
         IndexType* out_num_cubes,
         int device
     );
+
+    template <typename Scalar, typename IndexType>
+    void pc_to_voxel_grid_chunk(
+        Vertex<Scalar> const *points,
+        IndexType num_points,
+        IndexType res_x, 
+        IndexType res_y, 
+        IndexType res_z,
+        int chunk_size, 
+        int k_threshold,
+        int num_keep,
+        float rmi_x, float rmi_y, float rmi_z, float rma_x, float rma_y, float rma_z,
+        Vertex<Scalar>** out_vertices,
+        IndexType* out_num_vertices,
+        IndexType** out_cubes,
+        IndexType* out_num_cubes,
+        int device
+    );
 }
