55b532c338a19e816580b41479dac318ae4b305b
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-27 23:35:52 +0100

    refactor: update MC class to use voxel terminology in mc.h

--- march/csrc/mc.h
+++ march/csrc/mc.h
@@ -61,23 +61,23 @@ namespace mc {
     template <typename Scalar, typename IndexType>
     struct MC {
 
-        IndexType n_used_cubes{0};
+        IndexType n_used_voxels{0};
         IndexType n_verts{0};
         IndexType n_tris{0};
 
         // Temp storage
-        size_t allocated_cube_count{};
+        size_t allocated_voxel_count{};
         IndexType *__restrict__ temp_buffer{};
 
-        // cube
-        uint8_t *__restrict__ cube_codes{};           // cube code for each cell
+        // voxel
+        uint8_t *__restrict__ voxel_codes{};           // voxel code for each cell
 
-        // used cube
-        size_t allocated_used_cube_count{};
-        uint8_t *__restrict__ used_cube_code{};           // used cube to cube code
-        IndexType *__restrict__ used_cube_index{};        // used cube to cube index
-        IndexType *__restrict__ cube_edge_to_vert_idx{};    // cube to unique edge index
-        IndexType *__restrict__ used_to_first_mc_tri{};   // used cube to mc tri index
+        // used voxel
+        size_t allocated_used_voxel_count{};
+        uint8_t *__restrict__ used_voxel_code{};           // used voxel to voxel code
+        IndexType *__restrict__ used_voxel_index{};        // used voxel to voxel index
+        IndexType *__restrict__ voxel_edge_to_vert_idx{};    // voxel to unique edge index
+        IndexType *__restrict__ used_to_first_mc_tri{};   // used voxel to mc tri index
 
         // unique edge to vert
         EdgeKey<IndexType> *__restrict__ unique_edges{}; // [n_verts] array storing the pair of grid vertex indices
@@ -89,17 +89,17 @@ namespace mc {
         Vertex<Scalar> *__restrict__ out_colors{}; // output vert colors
         IndexType *__restrict__ tris{}; // output triangles
 
-        __host__ void ensure_grid_storage_size(size_t n_cubes);
-        __host__ void ensure_used_cube_storage_size(size_t n_used_cubes);
+        __host__ void ensure_grid_storage_size(size_t n_voxels);
+        __host__ void ensure_used_voxel_storage_size(size_t n_used_voxels);
         __host__ void ensure_vert_storage_size(size_t n_verts, bool with_colors);
         __host__ void ensure_tri_storage_size(size_t n_tris);
 
         __host__ void forward(
             Vertex<Scalar> const *grid_vertices, // N * 3 array of grid vertex positions
             Vertex<Scalar> const *grid_colors, // N * 3 array of grid vertex colors
-            IndexType const *cubes, // (N-1) * 8 array of cube vertex indices
+            IndexType const *voxels, // (N-1) * 8 array of voxel vertex indices
             Scalar const *values, // N array of scalar values at grid vertices
-            IndexType n_cubes, 
+            IndexType n_voxels, 
             Scalar iso, 
             int device
         );
@@ -118,10 +118,10 @@ namespace mc {
 
         __host__ ~MC() {
             if (temp_buffer) cudaFree(temp_buffer);
-            if (cube_codes) cudaFree(cube_codes);
-            if (used_cube_code) cudaFree(used_cube_code);
-            if (used_cube_index) cudaFree(used_cube_index);
-            if (cube_edge_to_vert_idx) cudaFree(cube_edge_to_vert_idx);
+            if (voxel_codes) cudaFree(voxel_codes);
+            if (used_voxel_code) cudaFree(used_voxel_code);
+            if (used_voxel_index) cudaFree(used_voxel_index);
+            if (voxel_edge_to_vert_idx) cudaFree(voxel_edge_to_vert_idx);
             if (used_to_first_mc_tri) cudaFree(used_to_first_mc_tri);
             if (unique_edges) cudaFree(unique_edges);
             if (verts) cudaFree(verts);
