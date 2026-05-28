52667150ab82a6d7bc96226023b699827fe768db
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-28 23:08:02 +0100

    refactor: update grid.h with latest voxel terminology

--- march/csrc/grid.h
+++ march/csrc/grid.h
@@ -1,43 +1,47 @@
+#ifndef VOXEL_GRID_H
+#define VOXEL_GRID_H
+
 #include "primitive.h"
 #include <cstdint>
 #include <cuda_runtime.h>
 
 using primitive::Vertex;
 
-namespace grid {
+namespace grid
+{
 
     template <typename Scalar, typename IndexType>
     void pc_to_voxel_grid(
         Vertex<Scalar> const *points,
         IndexType num_points,
-        IndexType res_x, 
-        IndexType res_y, 
-        IndexType res_z, 
+        IndexType res_x,
+        IndexType res_y,
+        IndexType res_z,
         int k_threshold,
         int num_keep,
         float rmi_x, float rmi_y, float rmi_z, float rma_x, float rma_y, float rma_z,
-        Vertex<Scalar>** out_vertices,
-        IndexType* out_num_vertices,
-        IndexType** out_voxels,
-        IndexType* out_num_voxels,
-        int device
-    );
+        Vertex<Scalar> **out_vertices,
+        IndexType *out_num_vertices,
+        IndexType **out_voxels,
+        IndexType *out_num_voxels,
+        int device);
 
     template <typename Scalar, typename IndexType>
     void pc_to_voxel_grid_chunk(
         Vertex<Scalar> const *points,
         IndexType num_points,
-        IndexType res_x, 
-        IndexType res_y, 
+        IndexType res_x,
+        IndexType res_y,
         IndexType res_z,
-        int chunk_size, 
+        int chunk_size,
         int k_threshold,
         int num_keep,
         float rmi_x, float rmi_y, float rmi_z, float rma_x, float rma_y, float rma_z,
-        Vertex<Scalar>** out_vertices,
-        IndexType* out_num_vertices,
-        IndexType** out_voxels,
-        IndexType* out_num_voxels,
-        int device
-    );
+        Vertex<Scalar> **out_vertices,
+        IndexType *out_num_vertices,
+        IndexType **out_voxels,
+        IndexType *out_num_voxels,
+        int device);
 }
+
+#endif // VOXEL_GRID_H
