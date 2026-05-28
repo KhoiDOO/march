38c92ab9feac2fe74450d828ebd5957ec6e6210b
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-28 23:08:10 +0100

    refactor: update primitive.h with kernel definitions

--- march/csrc/primitive.h
+++ march/csrc/primitive.h
@@ -1,3 +1,6 @@
+#ifndef PRIMITIVE_H
+#define PRIMITIVE_H
+
 #pragma once
 
 #include <cstdint>
@@ -15,7 +18,8 @@
 //    y
 //
 
-namespace primitive {
+namespace primitive
+{
 
     template <typename T>
     struct Vertex
@@ -24,44 +28,53 @@ namespace primitive {
 
         inline __device__ __host__ T *data_ptr() { return &x; }
 
-        inline __device__ __host__ Vertex<T> operator+(Vertex<T> const &other) const {
+        inline __device__ __host__ Vertex<T> operator+(Vertex<T> const &other) const
+        {
             return {x + other.x, y + other.y, z + other.z};
         }
-        inline __device__ __host__ T dot(Vertex<T> const &other) const {
+        inline __device__ __host__ T dot(Vertex<T> const &other) const
+        {
             return x * other.x + y * other.y + z * other.z;
         }
 
-        inline __device__ __host__ Vertex<T> operator-(Vertex<T> const &other) const {
+        inline __device__ __host__ Vertex<T> operator-(Vertex<T> const &other) const
+        {
             return {x - other.x, y - other.y, z - other.z};
         }
 
-        inline __device__ __host__ Vertex<T> operator*(Vertex<T> const &other) const {
+        inline __device__ __host__ Vertex<T> operator*(Vertex<T> const &other) const
+        {
             return {x * other.x, y * other.y, z * other.z};
         }
 
-        inline __device__ __host__ Vertex<T> operator*(T const &scalar) const {
+        inline __device__ __host__ Vertex<T> operator*(T const &scalar) const
+        {
             return {x * scalar, y * scalar, z * scalar};
         }
 
-        inline __device__ __host__ Vertex<T> operator/(T const &scalar) const {
+        inline __device__ __host__ Vertex<T> operator/(T const &scalar) const
+        {
             return {x / scalar, y / scalar, z / scalar};
         }
 
-        inline __device__ __host__ Vertex<T> &operator+=(Vertex<T> const &other) {
+        inline __device__ __host__ Vertex<T> &operator+=(Vertex<T> const &other)
+        {
             x += other.x;
             y += other.y;
             z += other.z;
             return *this;
         }
 
-        inline __device__ __host__ Vertex<T> &operator-=(Vertex<T> const &other) {
+        inline __device__ __host__ Vertex<T> &operator-=(Vertex<T> const &other)
+        {
             x -= other.x;
             y -= other.y;
             z -= other.z;
             return *this;
         }
 
-        inline __device__ __host__ Vertex<T> &operator*=(T const &scalar) {
+        inline __device__ __host__ Vertex<T> &operator*=(T const &scalar)
+        {
             x *= scalar;
             y *= scalar;
             z *= scalar;
@@ -70,27 +83,38 @@ namespace primitive {
     };
 
     template <typename T>
-    struct Triangle {
+    struct Triangle
+    {
         T i, j, k;
         inline __device__ __host__ T *data_ptr() { return &i; }
     };
 
     template <typename T>
-    struct BBox {
+    struct Quad
+    {
+        T i, j, k, l;
+        inline __device__ __host__ T *data_ptr() { return &i; }
+    };
+
+    template <typename T>
+    struct BBox
+    {
         Vertex<T> min_pt, max_pt;
 
         // A helper to initialize a BBox from a single point
-        inline static __device__ __host__ BBox<T> from_point(const Vertex<T>& pt) {
+        inline static __device__ __host__ BBox<T> from_point(const Vertex<T> &pt)
+        {
             return {pt, pt};
         }
 
         // A helper to merge two bounding boxes
-        inline __device__ __host__ BBox<T> merge(const BBox<T>& other) const {
+        inline __device__ __host__ BBox<T> merge(const BBox<T> &other) const
+        {
             BBox<T> res;
             res.min_pt.x = min(min_pt.x, other.min_pt.x);
             res.min_pt.y = min(min_pt.y, other.min_pt.y);
             res.min_pt.z = min(min_pt.z, other.min_pt.z);
-            
+
             res.max_pt.x = max(max_pt.x, other.max_pt.x);
             res.max_pt.y = max(max_pt.y, other.max_pt.y);
             res.max_pt.z = max(max_pt.z, other.max_pt.z);
@@ -99,22 +123,26 @@ namespace primitive {
     };
 
     template <typename IndexType>
-    struct EdgeKey {
+    struct EdgeKey
+    {
         IndexType v0;
         IndexType v1;
 
         // Required by thrust::sort and binary search
-        __host__ __device__
-        bool operator<(const EdgeKey& other) const {
-            if (v0 != other.v0) return v0 < other.v0;
+        __host__ __device__ bool operator<(const EdgeKey &other) const
+        {
+            if (v0 != other.v0)
+                return v0 < other.v0;
             return v1 < other.v1;
         }
 
         // Required by thrust::unique
-        __host__ __device__
-        bool operator==(const EdgeKey& other) const {
+        __host__ __device__ bool operator==(const EdgeKey &other) const
+        {
             return v0 == other.v0 && v1 == other.v1;
         }
     };
 
-}  // namespace primitive
+} // namespace primitive
+
+#endif // PRIMITIVE_H
