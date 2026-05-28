a4e275107378fbf17e71bc9a86be8163c0cd835a
Do Hoang Khoi <khoido8899@gmail.com>
2026-05-28 23:08:10 +0100

    refactor: update ops.h with latest kernels and operations

--- march/csrc/ops.h
+++ march/csrc/ops.h
@@ -4,11 +4,11 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 #include <stdint.h>
-#include "constants/mc_tables_y.h"
 
-bool check_cuda_result(cudaError_t code, const char *file, int line)
+inline bool check_cuda_result(cudaError_t code, const char *file, int line)
 {
-    if (code == cudaSuccess) return true;
+    if (code == cudaSuccess)
+        return true;
 
     fprintf(stderr, "CUDA error %u: %s (%s:%d)\n", unsigned(code), cudaGetErrorString(code), file, line);
     return false;
@@ -23,23 +23,16 @@ template <typename T>
 inline __device__ __host__ T max(T a, T b) { return a > b ? a : b; }
 
 template <typename T>
-inline __device__ __host__ T clamp(T x, T a, T b){ return min(max(a, x), b); }
+inline __device__ __host__ T clamp(T x, T a, T b) { return min(max(a, x), b); }
 
 template <typename T>
 inline __device__ __host__ T d_abs(T x) { return x < T(0.0) ? -x : x; }
 
-struct IsActiveOp {
-    __host__ __device__
-    int operator()(const uint8_t code) const {
+struct IsActiveOp
+{
+    __host__ __device__ int operator()(const uint8_t code) const
+    {
         return (code > 0 && code < 255) ? 1 : 0;
-    }
-};
-
-struct TriCountOp {
-    __device__
-    int operator()(const uint8_t code) const {
-        // Find the length using your standard offset table
-        return firstMarchingCubesId[code + 1] - firstMarchingCubesId[code];
     }
 };
 
